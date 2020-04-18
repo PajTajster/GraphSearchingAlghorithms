28c18c22c0df687eb6ac216571d433946f169ea1
PajTajster <38697671+PajTajster@users.noreply.github.com>
2020-04-18 16:32:04 +0200

    Added alghorithms interface for further expansion.

---
+++ GraphSearchingAlghorithms/PathfindingAlghorithms/Alghorithms/AlghorithmResult/AlghorithmCalculationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace PathfindingAlghorithms.Alghorithms.AlghorithmResult
+{
+    public abstract class AlghorithmCalculationResult
+    {
+        protected const string newLine = "\r\n";
+
+        public abstract override string ToString();
+    }
+}
---
+++ GraphSearchingAlghorithms/PathfindingAlghorithms/Alghorithms/Interface/IAlghorithm.cs
@@ -0,0 +1,10 @@
+﻿using PathfindingAlghorithms.Alghorithms.AlghorithmResult;
+using PathfindingAlghorithms.FileLoader;
+
+namespace PathfindingAlghorithms.Alghorithms.Interface
+{
+    public interface IAlghorithm
+    {
+        AlghorithmCalculationResult CalculateRoute(FileLoaderResult fileLoaderResult);
+    }
+}
